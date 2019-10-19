285ffcd2c83fb8aa7e7dcec39b18f4c22f9e8bf2
Tiago Antao <tiagoantao@gmail.com>
2019-10-19 18:02:47 -0400

    basic setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ty_cli',
+      version='0.1',
+      description='Easy command line interfaces in Python based on explict typing',
+      url='http://github.com/storborg/funniest',
+      author='Tiago Antao',
+      author_email='tiagoantao@gmail.com',
+      license='MIT',
+      packages=['ty_cli'],
+      zip_safe=False)

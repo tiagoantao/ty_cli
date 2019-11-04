5adf4e39a6e937b1d4fff7bec276ee004d22f19f
Tiago Rodrigues Antao <tiagoantao@gmail.com>
2019-11-04 18:22:44 -0500

    fun renaming

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,20 +1,20 @@
 import ty_cli
 
 
-def _fun_int_str(number: int) -> str:
+def _fun_int_to_str(number: int) -> str:
     return f'{number}'
 
 
 def test_basic():
-    wrapped_fun = ty_cli.cli(_fun_int_str)
+    wrapped_fun = ty_cli.cli(_fun_int_to_str)
     assert wrapped_fun(2) == '2'
 
 
 def test_signature():
-    wrapped_fun = ty_cli.cli(_fun_int_str)
-    assert wrapped_fun.__annotations__ == _fun_int_str.__annotations__
-    assert wrapped_fun.__defaults__ == _fun_int_str.__defaults__
+    wrapped_fun = ty_cli.cli(_fun_int_to_str)
+    assert wrapped_fun.__annotations__ == _fun_int_to_str.__annotations__
+    assert wrapped_fun.__defaults__ == _fun_int_to_str.__defaults__
 
 
 def test_create_argparse_from_function_signature():
-    ty_cli.create_argparse_from_function_signature(_fun_int_str)
+    ty_cli.create_argparse_from_function_signature(_fun_int_to_str)

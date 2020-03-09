fbfd17726fdeddbd2f288357327842b275c28870
Tiago Rodrigues Antao <tiagoantao@gmail.com>
2020-03-09 08:47:35 -0400

    Discussing invocation issues

--- examples/01_basic.py
+++ examples/01_basic.py
@@ -6,4 +6,7 @@ def int_to_str(number: int) -> str:
     return f"{number}"
 
 
+# This is not the best practice to invoke it, but serves to demonstrate
+# that the function signature is altered via the decorator.
+# See the 02_ example for the typical way to invoke cli
 int_to_str()

03025b545520cf623139338e97074c95bb027384
Tiago Rodrigues Antao <tiago@tiago.org>
2020-03-18 20:11:53 -0400

    testing optional and default parameters

---
+++ tests/test_optional_and_default.py
@@ -0,0 +1,24 @@
+import ty_cli
+
+
+def _fun_int_to_str(number: int) -> str:
+    """Some documentation"""
+    return f"{number}"
+
+
+def test_basic():
+    """Tests if a wrapped function, returns the expected value"""
+    wrapped_fun = ty_cli.cli(_fun_int_to_str)
+    assert wrapped_fun(2) == "2"
+
+
+def test_signature():  # Do we really want to do this?
+    """Test function signature"""
+    wrapped_fun = ty_cli.cli(_fun_int_to_str)
+    assert wrapped_fun.__annotations__ == _fun_int_to_str.__annotations__
+    assert wrapped_fun.__defaults__ == _fun_int_to_str.__defaults__
+    assert wrapped_fun.__kwdefaults__ == _fun_int_to_str.__kwdefaults__
+
+
+def test_create_argparse_from_function_signature():
+    ty_cli.create_argparse_from_function_signature(_fun_int_to_str)

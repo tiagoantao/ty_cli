f264ff8509dd1627ebdf7c10d19eb8721ef20ea0
Tiago Rodrigues Antao <tiago@tiago.org>
2020-03-19 21:04:43 -0400

    kw only tests

--- tests/test_kw_only.py
+++ tests/test_kw_only.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Optional
 
 import ty_cli
 
@@ -8,16 +9,31 @@ def _fun_int_to_str_kw(*, number: int) -> str:
     return f"{number}"
 
 
+def _fun_int_to_str_kw_optional(*, number: Optional[int]) -> str:
+    """Parameter has to be named"""
+    if number is None:
+        return "Nothing here"
+    else:
+        return f"{number}"
+
+
 def _fun_add_int_to_str_kw(*, number1: int, number2: int = 0) -> str:
     """Integer sum to String"""
     return f"{number1 + number2}"
 
 
-def test_kwargs():
+def test_kwonly():
     """Tests keyowrd only call"""
     ty_cli._clean_call_dictionary()
-    wrapped_fun = ty_cli.cli(_fun_int_to_str_kw)
-    assert wrapped_fun(number=2) == "2"
+    wrapped_fun = ty_cli.cli(_fun_add_int_to_str_kw)
+    assert wrapped_fun(number1=2) == "2"
+
+
+def test_kwonly_optional():
+    """Tests keyowrd only call"""
+    ty_cli._clean_call_dictionary()
+    wrapped_fun = ty_cli.cli(_fun_int_to_str_kw_optional)
+    assert wrapped_fun(number=None) == "Nothing here"
 
 
 def test_main_invocation():
@@ -27,5 +43,9 @@ def test_main_invocation():
     # Optional example
     ty_cli._clean_call_dictionary()
     ty_cli.cli(_fun_add_int_to_str_kw)
+    ty_cli.cli()
+    ty_cli._clean_call_dictionary()
+    sys.argv = ['TEST']
+    ty_cli.cli(_fun_int_to_str_kw_optional)
     ty_cli.cli()
 
